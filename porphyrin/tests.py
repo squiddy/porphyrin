3c38505fe35105b1d26f2cece5eff11266af3219
Reiner Gerecke <me@reinergerecke.de>
2017-07-17 19:26:27 +0200

    Add simple tests for web frontend

--- porphyrin/tests.py
+++ porphyrin/tests.py
@@ -2,7 +2,15 @@ import json
 from io import StringIO
 from unittest import mock
 
+import pytest
+
+from . import app
 from .atc_catalog import AtcCode, convert_data
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
 
 
 def test_atc_code_levels():
@@ -48,3 +56,29 @@ CA014A2,,This is another test,,
     assert json.load(target) == {
         'AB01432': 'This is a test',
         'CA014A2': 'This is another test'}
+
+
+def test_index(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
+
+
+@mock.patch('porphyrin.views.catalog')
+@mock.patch('porphyrin.views.naposapi')
+def test_lookup(mock_napos, mock_catalog, client):
+    mock_catalog.lookup.return_value = AtcCode('ABAAC01', 'name', mock_catalog)
+
+    rv = client.get('/classification/lookup/?atc_code=ABAAC01')
+    assert rv.status_code == 200
+    mock_napos.lookup_classification.assert_called_once_with('ABAAC01')
+    mock_catalog.lookup.assert_has_calls([
+        mock.call('ABAAC01'),
+        mock.call('A'),
+        mock.call('A'),
+        mock.call('ABA'),
+        mock.call('ABA'),
+        mock.call('ABAA'),
+        mock.call('ABAA'),
+        mock.call('ABAAC'),
+        mock.call('ABAAC')
+    ])
